b14dc3615c532e6008db46518a96d2f7a4c078b3
murilofarias <farias.murilo@gmail.com>
2021-07-07 10:05:31 -0300

    subindo meu primeiro projeto com Angular

---
+++ ProjetoComAngular/curso-manager/src/app/Courses/course-list.component.ts
@@ -0,0 +1,15 @@
+
+import { Component } from "@angular/core";
+
+
+@Component({
+    
+    selector: 'app-course-list',
+    template: '<h2> Course List </h2>'
+
+})
+
+    
+      export class CourseListComponent {
+        
+      }
