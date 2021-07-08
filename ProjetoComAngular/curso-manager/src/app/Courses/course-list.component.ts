d870d7d67bf7b4252c0248c8af2029160a15b44c
murilofarias10 <farias.murilo@gmail.com>
2021-07-08 12:06:26 -0300

    incluindo algumas alteracoes do curso ex bootstrap e outros

--- ProjetoComAngular/curso-manager/src/app/Courses/course-list.component.ts
+++ ProjetoComAngular/curso-manager/src/app/Courses/course-list.component.ts
@@ -1,15 +1,20 @@
+/* antes o arquivo era estatico
+@Component({
+        selector: 'app-course-list',
+    template: '<h2> Course List </h2>'
+})
+agora mudamos para ler um arquivo html
+*/
+
+
+
 
 import { Component } from "@angular/core";
-
-
 @Component({
-    
-    selector: 'app-course-list',
-    template: '<h2> Course List </h2>'
-
+        selector: 'app-course-list',
+    templateUrl : './course-list.component.html'
 })
 
-    
-      export class CourseListComponent {
+          export class CourseListComponent {
         
       }
