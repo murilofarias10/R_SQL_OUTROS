c9485db8e5ee6b32036f86ed7ca99f3b677f7e42
murilofarias10 <farias.murilo@gmail.com>
2021-07-11 10:46:28 -0300

    incluindo classe e criando uma tabela com esta classe 11.07.21

--- ProjetoComAngular/curso-manager/src/app/Courses/course-list.component.ts
+++ ProjetoComAngular/curso-manager/src/app/Courses/course-list.component.ts
@@ -6,15 +6,42 @@
 agora mudamos para ler um arquivo html
 */
 
+import {Component, OnInit } from "@angular/core";
+import {Course} from './course';
 
-
-
-import { Component } from "@angular/core";
 @Component({
         selector: 'app-course-list',
     templateUrl : './course-list.component.html'
 })
 
-          export class CourseListComponent {
-        
+/*
+export class CourseListComponent implements OnInit {
+        */
+
+export class CourseListComponent implements OnInit {
+    courses: Course[] = []; 
+    ngOnInit(): void {
+    this.courses = [
+            {
+                id: 1,
+                name: 'Murilo',
+                imageUrl: '',
+                price: 99.99,
+                code: 'XPS-8799',
+                duration: 120,
+                rating: 4.5,
+                releaseDate: 'November, 10, 2021'
+            },
+            {
+                id: 2,
+                name: 'Thamie',
+                imageUrl: '',
+                price: 49.99,
+                code: 'LKL-8799',
+                duration: 80,
+                rating: 5.4,
+                releaseDate: 'November, 10, 2021'
+             }
+                    ]
+}
       }
---
+++ ProjetoComAngular/curso-manager/src/app/Courses/course.ts
@@ -0,0 +1,12 @@
+export class Course {
+
+    id!: number;
+    name!: string;
+    imageUrl!: string;
+    price!: number;
+    code!: string;
+    duration!: number;
+    rating!: number;
+    releaseDate!: string;
+
+}
