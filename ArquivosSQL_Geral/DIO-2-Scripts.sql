a1b8304f16e36f2f3f8225e2a55549771f12c43c
Murilo Farias <79143042+murilofarias10@users.noreply.github.com>
2024-06-28 17:27:44 -0300

    28/06/2024
    -- Analisando Custumer and Invoice
    -- Analisando países
    -- Analisando por estado
    -- Compras total por usuario
    -- Ordernador Compras dia mes ano
    -- Verificando tabela Employee
    -- Nenhum de employee está em Customer, validação: Nome Completo
    -- Avaliando utilizando somente o first name para company null
    -- Validando o porque é errado utilizar somente o FIST NAME

--- ArquivosSQL_Geral/DIO-2-Scripts.sql
+++ ArquivosSQL_Geral/DIO-2-Scripts.sql
@@ -75,3 +75,146 @@ ORDER BY Name ASC
 )
 GROUP BY Name
 ORDER BY Name ASC
+
+
+ SELECT COUNT(Name) AS TOTAL_LINHAS, COUNT(DISTINCT Name) AS TOTAL_DISTINTOS
+ 	FROM(
+		 SELECT 
+		 	Album.ArtistId, Album.Title, Album.Column1,
+		 	Artist.Name
+		 FROM Album Album, Artist Artist
+		 WHERE Album.ArtistId = Artist.ArtistId 
+		 AND Album.Column1 IS NULL
+)
+
+
+--Analisando Custumer and Invoice
+SELECT COUNT(*) AS TOTAL_LINHAS FROM Customer c 
+
+--Analisando países
+SELECT * FROM Customer c 
+WHERE Country  = 'Brazil'
+
+SELECT State, COUNT(State) FROM Customer c 
+WHERE Country  = 'Brazil'
+GROUP BY State
+ORDER BY State 
+
+SELECT Country, COUNT(Country) AS TOTAL FROM Customer c 
+GROUP BY Country
+ORDER BY TOTAL DESC
+
+--Analisando por estado
+SELECT State, COUNT(*) AS TOTAL FROM Customer c 
+GROUP BY State
+ORDER BY TOTAL DESC
+
+SELECT 
+	c.CustomerId, c.FirstName, c.LastName, c.Country,
+	i.InvoiceDate, i.Total 
+FROM Customer c, Invoice i 
+WHERE 
+	c.CustomerId = i.CustomerId 
+ORDER BY c.CustomerId
+
+
+--Compras linha x linha
+SELECT 
+	c.FirstName, c.LastName, c.Country,
+	i.CustomerId, i.Total 
+FROM Customer c, Invoice i
+WHERE 
+	c.CustomerId = i.CustomerId 
+ORDER BY c.CustomerId ASC
+
+SELECT FirstName,  strftime('%m', InvoiceDate) AS MES, TOTAL
+FROM(
+	SELECT 
+	c.CustomerId, c.FirstName, c.LastName, c.Country,
+	i.InvoiceDate, SUM(i.Total) AS TOTAL 
+FROM Customer c, Invoice i 
+WHERE 
+	c.CustomerId = i.CustomerId 
+GROUP BY c.CustomerId 
+) AS SUBQUERIE 
+
+
+-- Compras total por usuario       
+SELECT  c.CustomerId, c.FirstName, SUM(i.Total) AS TOTAL 
+FROM    Customer c, Invoice i 
+WHERE   c.CustomerId = i.CustomerId 
+GROUP BY c.CustomerId
+ORDER BY TOTAL DESC 
+
+-- ordernador Compras dia mes ano
+SELECT 
+	CustomerId,
+	FirstName, Total,
+	strftime("%d", InvoiceDate) AS DIA, 
+	strftime("%m", InvoiceDate) AS MES, 
+	strftime("%Y", InvoiceDate) AS ANO
+FROM(
+SELECT 
+	c.FirstName, c.LastName, c.Country,
+	i.CustomerId, i.Total, i.InvoiceDate 
+FROM Customer c, Invoice i
+WHERE 
+	c.CustomerId = i.CustomerId 
+ORDER BY c.FirstName ASC
+) AS SUBQUERIES
+
+SELECT FirstName, COUNT(FirstName) AS QUANTIDADE FROM Customer c 
+GROUP BY FirstName 
+ORDER BY QUANTIDADE DESC
+
+
+SELECT 
+	FirstName, SUM(Total) AS TOTAL, 
+	strftime("%d", InvoiceDate) AS DIA, 
+	strftime("%m", InvoiceDate) AS MES, 
+	strftime("%Y", InvoiceDate) AS ANO
+FROM(
+SELECT 
+	c.FirstName, c.LastName, c.Country,
+	i.CustomerId, i.Total, i.InvoiceDate 
+FROM Customer c, Invoice i
+WHERE 
+	c.CustomerId = i.CustomerId 
+ORDER BY c.CustomerId ASC
+) AS SUBQUERIES
+GROUP BY FirstName
+ORDER BY TOTAL DESC
+
+--Verificando tabela Employee
+SELECT * FROM Customer c 
+SELECT * FROM Invoice i 
+SELECT * FROM Employee e 
+
+--Nenhum de employee está em Customer, validação: Nome Completo
+SELECT c.FirstName || ' ' || c.LastName  AS FULLNAME FROM Customer c
+ORDER BY FULLNAME
+
+SELECT e.FirstName || ' ' || e.LastName  AS FULLNAME FROM Employee e
+ORDER BY FULLNAME
+
+SELECT
+	c.FirstName || ' ' || c.LastName AS NOME_COMPLETO_CUSTOMER,
+	e.FirstName || ' ' || e.LastName AS NOME_COMPLETO_EMPLOYEE
+FROM Employee e, Customer c
+WHERE NOME_COMPLETO_CUSTOMER = NOME_COMPLETO_EMPLOYEE
+
+-- Avaliando utilizando somente o first name para company null
+SELECT
+	c.FirstName AS FIRST_C,
+	c.LastName  AS LAST_C,
+	e.FirstName AS FIRST_E,
+	e.LastName AS LAST_E,
+	c.Company 
+FROM Employee e, Customer c
+WHERE FIRST_C = FIRST_E
+
+--Validando o porque é errado utilizar somente o FIST NAME
+SELECT FirstName, LastName FROM Employee e WHERE FirstName = 'Steve' OR FirstName = 'Robert'
+
+SELECT FirstName, LastName FROM Customer c WHERE FirstName = 'Steve' OR FirstName = 'Robert'
+
