b6737e4daf5f483423977ac3d179b31690f86744
Murilo Farias <79143042+murilofarias10@users.noreply.github.com>
2024-10-30 09:52:22 -0300

    Update Arquivo
    Atualizando script realizando o exercicio 6

--- ArquivosSQL_Geral/16-DSA-Cap06-Exercicio6.sql
+++ ArquivosSQL_Geral/16-DSA-Cap06-Exercicio6.sql
@@ -1,6 +1,5 @@
 # SQL Para Análise de Dados e Data Science - Capítulo 06
 
-
 -- Criando a tabela 
 CREATE TABLE cap06.vendas_loja_online (
     id_cliente INT PRIMARY KEY,
@@ -10,8 +9,6 @@ CREATE TABLE cap06.vendas_loja_online (
     total_gasto_ultimo_mes DECIMAL(10,2),
     fez_compra_mes_atual BOOLEAN
 );
-
-
 
 -- Inserindo registros na tabela 
 INSERT INTO cap06.vendas_loja_online (id_cliente, pais_cliente, visitas_ultimo_mes, compras_ultimo_mes, total_gasto_ultimo_mes, fez_compra_mes_atual) VALUES 
@@ -26,9 +23,34 @@ INSERT INTO cap06.vendas_loja_online (id_cliente, pais_cliente, visitas_ultimo_m
 (1008, 'Canadá', 'sim', '11-15', 90.00, FALSE),
 (1009, 'Inglaterra', 'sim', '6-10', 179.30, TRUE);
 
+SELECT * FROM cap06.vendas_loja_online LIMIT 10;
 
+--Gerar uma nova tabela convertendo todas as variáveis categóricas 
+--para a representação  numérica.  
+--A  partir  da  variável pais_clientevocê  deve  criar  3  novas  colunas dummy.
 
-
-
-
-
+SELECT
+	id_cliente,
+	CASE
+		WHEN pais_cliente = 'Brasil' THEN 1 ELSE 0
+	END as cliente_Brasil,
+	CASE
+		WHEN pais_cliente = 'Canadá' THEN 1 ELSE 0
+	END as cliente_Canadá,
+	CASE
+		WHEN pais_cliente = 'Inglaterra' THEN 1 ELSE 0
+	END as cliente_Inglaterra,
+	CASE
+		WHEN visitas_ultimo_mes = 'sim' THEN 1 ELSE 0
+	END as visitas_ultimo_mes,
+	CASE
+		WHEN compras_ultimo_mes = '0-5' OR compras_ultimo_mes = '6-10' THEN 1
+		WHEN compras_ultimo_mes = '11-15' THEN 2
+		WHEN compras_ultimo_mes = '16-20' THEN 3
+		ELSE 4
+	END as grupo_compras_ultimo_mes,
+	total_gasto_ultimo_mes,
+	CASE
+		WHEN fez_compra_mes_atual = 'true' THEN 1 ELSE 0
+	END as compra_mes_atual
+FROM cap06.vendas_loja_online;
