a6fae922bda5fd163fd17eafe299224f0a9732f4
Murilo Farias <79143042+murilofarias10@users.noreply.github.com>
2024-11-29 14:40:41 -0300

    update arquivo SQL
    Atualizando arquivo curso, exercicios

--- ArquivosSQL_Geral/19-DSA-Cap09-Exercicios1.sql
+++ ArquivosSQL_Geral/19-DSA-Cap09-Exercicios1.sql
@@ -38,10 +38,13 @@ SELECT COUNT(id_cliente) as "Total Pedidos", id_cliente FROM cap09.dsa_pedidos
 GROUP BY id_cliente
 ORDER BY "Total Pedidos" DESC
 
-
-
-
-
+--Contagem de pedido de clientes do estado do RJ ou de SP
+SELECT cli.estado_cliente AS Estado, COUNT(pe.id_pedido) AS "Total Clientes RJ e SP "
+FROM cap09.dsa_pedidos pe
+INNER JOIN cap09.dsa_clientes cli
+ON pe.id_cliente = cli.id_cli
+WHERE cli.estado_cliente IN ('SP', 'RJ')
+GROUP BY Estado
 
 -- Contagem de pedidos de clientes do estado do RJ ou SP (esta query está correta?)
 SELECT estado_cliente, COUNT(*) AS total_pedidos
@@ -49,7 +52,6 @@ FROM cap09.dsa_pedidos AS P, cap09.dsa_clientes AS C
 WHERE P.id_cliente = C.id_cli
 AND estado_cliente = 'RJ' OR estado_cliente = 'SP'
 GROUP BY estado_cliente;
-
 
 -- Visualizar os dados
 SELECT * 
@@ -60,14 +62,12 @@ SELECT *
 FROM cap09.dsa_pedidos
 ORDER BY id_cliente;
 
-
 -- Contagem de pedidos de clientes do estado do RJ ou SP (agora sim!)
 SELECT estado_cliente, COUNT(*) AS total_pedidos
 FROM cap09.dsa_pedidos AS P, cap09.dsa_clientes AS C
 WHERE P.id_cliente = C.id_cli
 AND (estado_cliente = 'RJ' OR estado_cliente = 'SP')
 GROUP BY estado_cliente
-
 
 -- Contagem de pedidos de clientes do estado do RJ ou SP (alternativa)
 SELECT C.estado_cliente, COUNT(P.id_pedido) AS total_pedidos
@@ -76,21 +76,33 @@ INNER JOIN cap09.dsa_pedidos P ON C.id_cli = P.id_cliente
 WHERE C.estado_cliente IN ('RJ', 'SP')
 GROUP BY C.estado_cliente;
 
-
 -- Contagem de pedidos de clientes do estado do RJ ou SP por nome de cliente
+SELECT * FROM cap09.dsa_clientes
+SELECT * FROM cap09.dsa_pedidos
+SELECT * FROM cap09.dsa_produtos
+
+--murilo
+SELECT COUNT(pe.id_pedido) as "TOTAL PEDIDOS", cli.nome_cliente, cli.estado_cliente
+FROM cap09.dsa_pedidos pe
+INNER JOIN cap09.dsa_clientes cli
+ON pe.id_cliente = cli.id_cli
+WHERE cli.estado_cliente IN ('RJ', 'SP')
+GROUP BY cli.nome_cliente, cli.estado_cliente
+
+--professor
 SELECT nome_cliente, estado_cliente, COUNT(*) AS total_pedidos
 FROM cap09.dsa_pedidos AS P, cap09.dsa_clientes AS C
 WHERE P.id_cliente = C.id_cli
 AND (estado_cliente = 'RJ' OR estado_cliente = 'SP')
 GROUP BY nome_cliente, estado_cliente;
 
-
 -- Maior valor 
 SELECT MAX(valor_pedido) AS maximo
 FROM cap09.dsa_pedidos;
 
+SELECT MAX(data_pedido) AS maximo
+FROM cap09.dsa_pedidos;
 
 -- Menor valor 
 SELECT MIN(valor_pedido) AS minimo
 FROM cap09.dsa_pedidos;
-
