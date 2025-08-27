51b793c3c12d4614afd979018f32960e5714325d
Murilo Farias <79143042+murilofarias10@users.noreply.github.com>
2025-08-27 15:31:49 -0700

    Update 34-DSA-LAB2-Perguntas.sql
    distribuicao dos dados
    using WITHIN GROUP, STDDEV para standard deviation

--- ArquivosSQL_Geral/34-DSA-LAB2-Perguntas.sql
+++ ArquivosSQL_Geral/34-DSA-LAB2-Perguntas.sql
@@ -2,8 +2,6 @@
 --A query deve mostrar: contagem_lançamentos, total_valores_lançamentos,
 --media_valores_lançamento, maior_valor, menor_valor,
 --soma_valores_usd, soma_valores_eur, soma_valores_brl, media_taxa_conversao e mediana_valores
-SELECT * FROM cap13.lancamentosdsacontabeis LIMIT 10
-
 SELECT 
 	centro_custo,
 	COUNT(*) AS contagem_lançamentos,
@@ -19,8 +17,28 @@ FROM cap13.lancamentosdsacontabeis
 GROUP BY centro_custo
 ORDER BY total_valores_lançamentos DESC
 
+
+--Distribuição de dados
+--Crie uma query para mostrar a distribuição de dados na tabela
+--Estamos interessandos na columa valor.
+-- O relatório deve mostrar: quantidade_lancamentos, media_valor, desvio_padrão_valor, menor_valor
+--maior_valor e priemiro, segunda e terceiro quartil
+-- faça tudo isso por centro de custo e por moeda
+SELECT * FROM 
+cap13.lancamentosdsacontabeis LIMIT 10
 	
+SELECT 
+	centro_custo, moeda, 
+	count(valor) as quantidade_lancamentos,
+	ROUND(STDDEV(valor)::NUMERIC, 2) as std,
+	ROUND(AVG(valor),2) as media,
+	MIN(valor) as menor_valor,
+	MAX(valor) as maior_valor,
+	ROUND(PERCENTILE_CONT(0.25) WITHIN GROUP (ORDER BY valor)::NUMERIC, 2) as first_quatile,
+	ROUND(PERCENTILE_CONT(0.50) WITHIN GROUP (ORDER BY valor)::NUMERIC, 2) as second_quatile,
+	ROUND(PERCENTILE_CONT(0.75) WITHIN GROUP (ORDER BY valor)::NUMERIC, 2) as third_quatile
+FROM cap13.lancamentosdsacontabeis
+GROUP BY centro_custo, moeda
+ORDER BY centro_custo, quantidade_lancamentos DESC
 
-
-
-
+	
