6bb092ef8c43be86c2d6b7c29c19dd7ff3577cd8
Murilo Farias <79143042+murilofarias10@users.noreply.github.com>
2025-09-04 11:04:29 -0700

    Update 35-DSA-LAB3-Perguntas.sql
    Using some CTE + CROSS JOIN f and others queries

--- ArquivosSQL_Geral/35-DSA-LAB3-Perguntas.sql
+++ ArquivosSQL_Geral/35-DSA-LAB3-Perguntas.sql
@@ -119,3 +119,95 @@ FROM cap15.dsa_campanha_marketing
 		nome_campanha, data_inicio, publico_alvo, canais_divulgacao
 	HAVING COUNT(*) > 1
 )
+
+--filtrando nao outliers
+-- Crie uma querie que identifique outliers nas 3 colunas numericas
+--media -1,5 * desvio padrão
+WITH original AS (
+SELECT orcamento, taxa_conversao, impressoes FROM cap15.dsa_campanha_marketing
+WHERE orcamento IS NOT NULL AND taxa_conversao IS NOT NULL AND impressoes IS NOT NULL
+),
+filtera AS (
+SELECT 
+	ROUND(AVG(orcamento) - (1.5 * STDDEV(orcamento)),2) AS orcamento_outmin,
+	ROUND(AVG(orcamento) + (1.5 * STDDEV(orcamento)),2) AS orcamento_outmxn,
+	ROUND(AVG(taxa_conversao) - (1.5 * STDDEV(taxa_conversao)),2) AS taxa_conversao_outmin,
+	ROUND(AVG(taxa_conversao) + (1.5 * STDDEV(taxa_conversao)),2) AS taxa_conversao_outmxn,
+	ROUND(AVG(impressoes) - (1.5 * STDDEV(impressoes)),2) AS impressoes_outmin,
+	ROUND(AVG(impressoes) + (1.5 * STDDEV(impressoes)),2) AS impressoes_outmxn
+FROM cap15.dsa_campanha_marketing
+)
+SELECT o.orcamento, o.taxa_conversao, o.impressoes
+FROM original o
+CROSS JOIN filtera f
+WHERE 
+	o.orcamento > f.orcamento_outmin AND o.orcamento < f.orcamento_outmxn
+	AND o.taxa_conversao > f.taxa_conversao_outmin AND o.taxa_conversao < f.taxa_conversao_outmxn
+	AND o.impressoes > f.impressoes_outmin AND o.impressoes < f.impressoes_outmxn
+ORDER BY o.orcamento DESC;
+
+--filter outliers
+WITH original AS (
+SELECT orcamento, taxa_conversao, impressoes FROM cap15.dsa_campanha_marketing
+WHERE orcamento IS NOT NULL AND taxa_conversao IS NOT NULL AND impressoes IS NOT NULL
+),
+filtera AS (
+SELECT 
+	ROUND(AVG(orcamento) - (1.5 * STDDEV(orcamento)),2) AS orcamento_outmin,
+	ROUND(AVG(orcamento) + (1.5 * STDDEV(orcamento)),2) AS orcamento_outmxn,
+	ROUND(AVG(taxa_conversao) - (1.5 * STDDEV(taxa_conversao)),2) AS taxa_conversao_outmin,
+	ROUND(AVG(taxa_conversao) + (1.5 * STDDEV(taxa_conversao)),2) AS taxa_conversao_outmxn,
+	ROUND(AVG(impressoes) - (1.5 * STDDEV(impressoes)),2) AS impressoes_outmin,
+	ROUND(AVG(impressoes) + (1.5 * STDDEV(impressoes)),2) AS impressoes_outmxn
+FROM cap15.dsa_campanha_marketing
+)
+SELECT o.orcamento, o.taxa_conversao, o.impressoes
+FROM original o
+CROSS JOIN filtera f
+WHERE 
+	o.orcamento < f.orcamento_outmin OR o.orcamento > f.orcamento_outmxn
+	OR o.taxa_conversao < f.taxa_conversao_outmin OR o.taxa_conversao > f.taxa_conversao_outmxn
+	OR o.impressoes < f.impressoes_outmin OR o.impressoes > f.impressoes_outmxn
+ORDER BY o.orcamento DESC;
+
+
+--Teacher answer
+WITH stats AS (
+	SELECT
+		AVG(orcamento) as avg_orcamento,
+		STDDEV(orcamento) AS stdev_orcamento,
+		AVG(taxa_conversao) as avg_taxa_conversao,
+		STDDEV(taxa_conversao) AS stdev_taxa_conversao,
+		AVG(impressoes) as avg_impressoes,
+		STDDEV(impressoes) AS stdev_impressoes
+FROM cap15.dsa_campanha_marketing
+)
+SELECT id, nome_campanha, data_inicio, data_fim, orcamento, publico_alvo, canais_divulgacao,
+taxa_conversao, impressoes
+FROM cap15.dsa_campanha_marketing, stats
+WHERE
+	orcamento < (avg_orcamento-1.5*stdev_orcamento) OR
+	orcamento > (avg_orcamento+1.5*stdev_orcamento) OR
+	taxa_conversao < (avg_taxa_conversao-1.5*stdev_taxa_conversao) OR
+	taxa_conversao > (avg_taxa_conversao+1.5*stdev_taxa_conversao) OR
+	impressoes < (avg_impressoes-1.5*stdev_impressoes) OR
+	impressoes > (avg_impressoes+1.5*stdev_impressoes)
+ORDER BY orcamento DESC
+
+
+SELECT
+	(avg_orcamento-1.5*stdev_orcamento) as min_orc,
+	(avg_orcamento+1.5*stdev_orcamento) as max_orc,
+	(avg_taxa_conversao-1.5*stdev_taxa_conversao) as min_taxa_conver,
+	(avg_taxa_conversao+1.5*stdev_taxa_conversao) as max_taxa_conver,
+	(avg_impressoes-1.5*stdev_impressoes) as min_impressoes,
+	(avg_impressoes+1.5*stdev_impressoes) as max_impressoes
+FROM(
+SELECT
+		AVG(orcamento) as avg_orcamento,
+		STDDEV(orcamento) AS stdev_orcamento,
+		AVG(taxa_conversao) as avg_taxa_conversao,
+		STDDEV(taxa_conversao) AS stdev_taxa_conversao,
+		AVG(impressoes) as avg_impressoes,
+		STDDEV(impressoes) AS stdev_impressoes
+FROM cap15.dsa_campanha_marketing ) AS SUB
